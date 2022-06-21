dbe34f2a30d8c6ceaf0ff990254a645fdb82ace9
Msdalmajdi <msdalmajdi@hotmail.com>
2022-06-21 16:58:43 +0300

    Add navigation to trip details

--- trips/components/navigation/Index.js
+++ trips/components/navigation/Index.js
@@ -1,31 +1,50 @@
-import * as React from 'react';
-import { useNavigation  } from '@react-navigation/native';
-import { SignInPage } from '../user/SignInPage';
+import * as React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SignInPage } from "../user/SignInPage";
 // import { SignUpPage } from '../user/SignUpPage';
-import {HelloWorldApp} from '../ph';
-import userStore from './../../stores/userStore';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HelloWorldApp } from "../ph";
+import TripsList from "../TripsList";
+import TripDetails from "../TripDetails";
+import userStore from "./../../stores/userStore";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { observer } from "mobx-react";
+import tripStore from "../../stores/tripStore";
 
+const { Navigator, Screen } = createNativeStackNavigator();
 
-const {Navigator,Screen} = createNativeStackNavigator();
-
- function Index() {
+function Index() {
   const navigation = useNavigation();
-  let isSignedIn = false
-  if(userStore.user) isSignedIn = true
+  let isSignedIn = false;
+  if (userStore.user) isSignedIn = true;
   return (
-    <Navigator initialRouteName="List" screenOptions={{headerTitleAlign: 'center'}}>
-   { isSignedIn ? (
-     <Screen name="Hello" component={HelloWorldApp} />
-  ) : (
-    <>
-    <Screen name="Signin" component={SignInPage} options={{headerShown: false}} />
-    {/* <Screen name="Signup" component={SignUpPage} options={{headerShown: false}}  /> */}
-    </>
-  ) }
-  </Navigator>
-
-  )
+    <Navigator
+      initialRouteName="List"
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
+      {isSignedIn ? (
+        <>
+          <Screen name="Hello" component={TripsList} />
+          <Screen
+            name="Trip-details"
+            options={({ route }) => {
+              return {
+                title: tripStore.getTripById(route.params._id).title,
+              };
+            }}
+            component={TripDetails}
+          />
+        </>
+      ) : (
+        <>
+          <Screen
+            name="Signin"
+            component={SignInPage}
+            options={{ headerShown: false }}
+          />
+          {/* <Screen name="Signup" component={SignUpPage} options={{headerShown: false}}  /> */}
+        </>
+      )}
+    </Navigator>
+  );
 }
-export default observer(Index)
+export default observer(Index);
